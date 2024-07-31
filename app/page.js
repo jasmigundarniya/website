709de913781a3965f5d8dc7fc3463ba18592b288
jasmi gundarniya <jasmigundarniya@gmail.com>
2024-07-31 22:09:18 +0530

    design login page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-
+import Login from "./login/page";
 const page = () => {
   return (
     <div>
-      <Header />
-      <Sidebar />
+      <Login />
+      {/* <Header />
+      <Sidebar /> */}
     </div>
   );
 };
